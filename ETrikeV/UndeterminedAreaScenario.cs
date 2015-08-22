2b2c3dc09bc5f1765895da97288ecd6798239e6d
unknown-user <unknown-user@fe80::7808:1ba6:166c:4b71%11>
2015-08-22 13:10:30 +0900

    仕様未確定エリア2用のシナリオ(ひな形だけ)

---
+++ ETrikeV/UndeterminedAreaScenario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ETrikeV
+{
+	public class UndeterminedAreaScenario: Scenario
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ETrikeV.UndeterminedAreaScenario"/> class.
+		/// </summary>
+		public UndeterminedAreaScenario ()
+		{
+			//規定処理は特になし
+		}
+
+		/// <summary>
+		/// 仕様未確定エリア2
+		/// </summary>
+		/// <param name="sys">Sys.</param>
+		public override bool run(Ev3System sys)
+		{
+			
+			return true;
+		}
+	}
+}
+
