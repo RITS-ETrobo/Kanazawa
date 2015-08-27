eb540c62b60e8dc09f6d6888f41b0d04732a204b
sakano <Hiroshi.Sakano@jrits.ricoh.co.jp>
2015-08-28 07:01:20 +0900

    バーコードシナリオの扱いを変更

--- ETrikeV/UndeterminedAreaScenario.cs
+++ ETrikeV/UndeterminedAreaScenario.cs
@@ -2,14 +2,24 @@
 
 namespace ETrikeV
 {
+	/// <summary>
+	/// 仕様未確定エリア(バーコードシナリオを内包)
+	/// </summary>
 	public class UndeterminedAreaScenario: Scenario
 	{
+		/// <summary>
+		/// バーコード用のシナリオ
+		/// </summary>
+		private BarcodeScenario theBarcode;
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ETrikeV.UndeterminedAreaScenario"/> class.
 		/// </summary>
 		public UndeterminedAreaScenario ()
 		{
-			//規定処理は特になし
+			// コンストラクタでメモリ確保はしたくないが、
+			// 直前だとディレイがありそうなので、事前に生成しておく
+			theBarcode = new BarcodeScenario();
 		}
 
 		/// <summary>
@@ -19,14 +29,29 @@ namespace ETrikeV
 		public override bool run(Ev3System sys)
 		{
 			//いやだけど、ここでバーコードの情報をもらわないとどうにもらなない
-			BarcodeScenario barcode = new BarcodeScenario();
-			barcode.run (sys);
-			int barcodeBit = barcode.getBarcodeBit ();
+			theBarcode.run (sys);
 
 			//ここから、仕様未確定エリアの動作
 
+			//ルート生成
+			createUndeterminedAreaRoot(sys);
+
+			//走行
+
+			//ライン復帰
 
 			return true;
+		}
+
+		/// <summary>
+		/// 仕様未確定エリアのルート生成
+		/// </summary>
+		/// <param name="sys"></param>
+		private void createUndeterminedAreaRoot(Ev3System sys)
+		{
+			int barcodeBit = theBarcode.getBarcodeBit();
+
+			return;
 		}
 	}
 }
