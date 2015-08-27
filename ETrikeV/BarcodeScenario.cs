e5321a5da02f235634f23a296a606948810577dd
sakano <Hiroshi.Sakano@jrits.ricoh.co.jp>
2015-08-27 21:08:38 +0900

    バーコードシナリオのひな型更新 バーコード情報を読み取るための、ゲッター作成

--- ETrikeV/BarcodeScenario.cs
+++ ETrikeV/BarcodeScenario.cs
@@ -2,10 +2,41 @@
 
 namespace ETrikeV
 {
-	public class BarcodeScenario
+	public class BarcodeScenario : Scenario
 	{
+		/// <summary>
+		/// 読み取ったバーコード情報
+		/// </summary>
+		int BarcodeBit = 0;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ETrikeV.BarcodeScenario"/> class.
+		/// </summary>
 		public BarcodeScenario ()
 		{
+			// 何もなし
+		}
+
+		/// <summary>
+		/// バーコード
+		/// </summary>
+		/// <param name="sys">Sys.</param>
+		public override bool run(Ev3System sys)
+		{
+			//段差検出
+			//バーコード開始位置検出(通常ライントレースしながら)
+			//3cm単位でバーコード読み取り(3cm×10回)
+			//終わったら、this.BarcodeBit に値を入れる
+			return true;
+		}
+
+		/// <summary>
+		/// バーコードの値
+		/// </summary>
+		/// <returns>The barcode bit.</returns>
+		public int getBarcodeBit()
+		{
+			return BarcodeBit;
 		}
 	}
 }
