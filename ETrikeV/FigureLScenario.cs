f754d48fbf796e608e47c030660ff972306b976f
iida <Takahiro.Iida@jrits.ricoh.co.jp>
2015-08-30 17:14:42 +0900

    フィギュアLの動作を変更する。
    　段差登り時：モーターパワーを変えて2段階で登る
    
    　右旋回時：ステアリングの角度を変えて、2段階で旋回する

--- ETrikeV/FigureLScenario.cs
+++ ETrikeV/FigureLScenario.cs
@@ -28,8 +28,16 @@ namespace ETrikeV
 			sys.setSteerSlope (slop);
 
 			tmpDistance[0] = sys.leftMotorGetMoveCm();
-			sys.setRightMotorPower(rightPw);
-			sys.setLeftMotorPower(leftPw);
+			if (rightPw == 0) {
+				sys.rightMotorBrake ();
+			} else {
+				sys.setRightMotorPower (rightPw);
+			}
+			if (leftPw == 0) {
+				sys.leftMotorBrake ();
+			} else {
+				sys.setLeftMotorPower (leftPw);
+			}
 
 			while (true)
 			{
@@ -44,72 +52,107 @@ namespace ETrikeV
 
 			sys.stopMotors ();
 		}
-			
+	
+
 		public override bool run(Ev3System sys)
 		{
 
-			//段差を検知するまでライントレースする
+			//段差を検知する
+			/*************************************************/
 			while (true) {
-				lineTrace(sys, 30, Mode.Left, LIGHT_WIDTH, MAX_STEERING_ANGLE, STEER_POWER);
-				if (isStep (sys) == true) {
-					break;
-				}
-				Thread.Sleep (5);
+			lineTrace(sys, 30, Mode.Left, LIGHT_WIDTH, MAX_STEERING_ANGLE, STEER_POWER);
+			if (isStep (sys) == true) {
+			sys.stopMotors ();
+			break;
 			}
+			Thread.Sleep (5);
+			}
+			/*************************************************/
 
+			//板を登る
+			/*************************************************/
 			//ステアリングの傾きを正面に修正する
+			actionStraight(sys, -1, 30);
 			sys.setSteerSlope (0);
-
-			//段差を超えるための助走分だけ後退する
-			actionStraight(sys, -5, 40);
 
 			//段差を超えるために高いスピードで前進する
-			actionStraight(sys, 22, 60); //70
+			actionStraight(sys, 5, 40); 
+			Thread.Sleep(10);
+			actionStraight(sys, 8, 70); 	//10
+			/*************************************************/
 
+			//板上のラインに復帰する
+			/*************************************************/
 			//板上のラインに復帰するために首を振ってライン探索する
-			serchLine (sys, 5, true);
+			serchLine (sys, 3, true);
 
 			//ステアリングの傾きを正面に修正する
 			sys.setSteerSlope (0);
+			/*************************************************/
 
+			//右旋回位置までライン上を移動する
+			/*************************************************/
 			//ライントレースで前進（規定距離だけ）
 			int nowDistance = sys.getAverageMoveCM();
 			while (true) {
-				lineTrace(sys, 50, Mode.Left, LIGHT_WIDTH, MAX_STEERING_ANGLE, STEER_POWER);
-				if (sys.getAverageMoveCM() > (nowDistance + 10)) { // 16
-					break;
-				}
-				Thread.Sleep (5);
+			lineTrace(sys, 50, Mode.Left, LIGHT_WIDTH, MAX_STEERING_ANGLE, STEER_POWER);
+			if (sys.getAverageMoveCM() > (nowDistance + 15)) { // 12
+			sys.stopMotors ();
+			Thread.Sleep(100);
+			break;
 			}
-				
-			//右に旋回
-			//actionRightTurn(sys, -50, 50, 30, 10);		//30度より高い角度だと進みすぎて精密な制御ができない
-			actionRightTurn(sys, 0, 70, 30, 21);		//20 それなり動いた
+			Thread.Sleep (5);
+			}
+			/*************************************************/
 
+			//2回に分けて右に旋回する
+			/*************************************************/
+			//1回目
+			actionRightTurn(sys, 0, 60, 30, 11);	//10
+			Thread.Sleep(100);
+
+			//位置修正
+			sys.setSteerSlope (0);
+			actionStraight(sys, 1, 20); 	//3
+			Thread.Sleep(100);
+
+			//2回目
+			actionRightTurn(sys, 10, 40, 60, 11);	//10
+			Thread.Sleep(100);
+			/*************************************************/
+
+			//板から下りる
+			/*************************************************/
 			//ステアリングの傾きを正面に修正する
 			sys.setSteerSlope (0);
 
 			//板から降りる
 			//斜め方向に進むので、完了後はライン探索が必須
-			actionStraight(sys, 15, 100); //15
+			actionStraight(sys, 10, 40); //15
+			/*************************************************/
 
+			//ライン上に復帰する
+			/*************************************************/
 			//板から下りてラインを探す
-			serchLine (sys, 15, true);
+			serchLine (sys, 5, true);
 
 			//ステアリングの傾きを正面に修正する
 			sys.setSteerSlope (0);
+			/*************************************************/
 
+			//新幹線手前までライン上を進む
+			/*************************************************/
 			//ライン復帰のためのライントレース
 			nowDistance = sys.getAverageMoveCM();
 			while (true) {
-				lineTrace(sys, 50, Mode.Left, LIGHT_WIDTH, MAX_STEERING_ANGLE, STEER_POWER);
-				if (sys.getAverageMoveCM() > (nowDistance + 8)) { // 7
-					break;
-				}
-				Thread.Sleep (5);
-			}
-
+			lineTrace(sys, 50, Mode.Left, LIGHT_WIDTH, MAX_STEERING_ANGLE, STEER_POWER);
+			if (sys.getAverageMoveCM() > (nowDistance + 8)) { // 7
 			sys.stopMotors ();
+			break;
+			}
+			Thread.Sleep (5);
+			}
+			/*************************************************/
 
 			return true;
 		}
