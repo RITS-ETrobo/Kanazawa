ea09c800930721f991f909d1e5406cbc15ab6cd4
kon <sound.recording.man@gmail.com>
2015-08-31 12:33:47 +0900

    コーナー確認用の処理を新規シナリオに移行

---
+++ ETrikeV/CornerCheckScenario.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using MonoBrickFirmware.Display;
+using MonoBrickFirmware.UserInput;
+
+namespace ETrikeV
+{
+	public class CornerCheckScenario : Scenario
+	{
+		private int outSpeed = 100;
+		private int inSpeed = 0;
+		private int turn = 0;
+		private bool end = false;
+
+		public CornerCheckScenario ()
+		{
+			ButtonEvents buts = new ButtonEvents();
+			buts.UpPressed += () => {
+				inSpeed += 5;
+				LcdConsole.Clear();
+				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
+			};
+			buts.DownPressed += () => {
+				inSpeed -= 5;
+				LcdConsole.Clear();
+				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
+			};
+			buts.EnterPressed += () => {
+				end = true;
+			};
+			buts.LeftPressed += () => {
+				turn -= 10;
+				LcdConsole.Clear();
+				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
+			};
+			buts.RightPressed += () => {
+				turn += 10;
+				LcdConsole.Clear();
+				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
+			};
+		}
+
+		public override bool run(Ev3System sys)
+		{
+			int current = turn;
+			sys.setSteerSlope (current);
+			while (!end) {
+				if (current < 0) {
+					sys.setLeftMotorPower (inSpeed);
+					sys.setRightMotorPower (outSpeed);
+				} else {
+					sys.setLeftMotorPower (outSpeed);
+					sys.setRightMotorPower (inSpeed);
+				}
+			}
+
+			sys.stopMotors ();
+			end = false;
+			Thread.Sleep (1000);
+
+			return false;
+		}
+	}
+}
+
--- ETrikeV/TestScenario.cs
+++ ETrikeV/TestScenario.cs
@@ -1,63 +1,15 @@
 ﻿using System;
-using System.Threading;
-using MonoBrickFirmware.UserInput;
-using MonoBrickFirmware.Display;
 
 namespace ETrikeV
 {
 	public class TestScenario : Scenario
 	{
-		private int outSpeed = 100;
-		private int inSpeed = 0;
-		private int turn = 0;
-		private bool end = false;
-
 		public TestScenario ()
 		{
-			ButtonEvents buts = new ButtonEvents();
-			buts.UpPressed += () => {
-				inSpeed += 5;
-				LcdConsole.Clear();
-				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
-			};
-			buts.DownPressed += () => {
-				inSpeed -= 5;
-				LcdConsole.Clear();
-				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
-			};
-			buts.EnterPressed += () => {
-				end = true;
-			};
-			buts.LeftPressed += () => {
-				turn -= 10;
-				LcdConsole.Clear();
-				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
-			};
-			buts.RightPressed += () => {
-				turn += 10;
-				LcdConsole.Clear();
-				LcdConsole.WriteLine("T:" + turn + " In:" + inSpeed + " Out:" + outSpeed);
-			};
 		}
 
 		public override bool run(Ev3System sys)
 		{
-			int current = turn;
-			sys.setSteerSlope (current);
-			while (!end) {
-				if (current < 0) {
-					sys.setLeftMotorPower (inSpeed);
-					sys.setRightMotorPower (outSpeed);
-				} else {
-					sys.setLeftMotorPower (outSpeed);
-					sys.setRightMotorPower (inSpeed);
-				}
-			}
-
-			sys.stopMotors ();
-			end = false;
-			Thread.Sleep (1000);
-
 			return false;
 		}
 	}
