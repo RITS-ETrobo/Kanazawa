0fbf35d5a18e56929662001796923047deeb860c
kon <sound.recording.man@gmail.com>
2015-09-02 18:22:49 +0900

    二本橋回避用のシナリオを追加（実装中）

---
+++ ETrikeV/BridgeEscapeScenario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ETrikeV
+{
+	public class BridgeEscapeScenario : Scenario
+	{
+		private enum BridgeEscapeState {Turn1, Straight1, Turn2, Straight2, Turn3, Straight3, Turn4, Line};
+		private BridgeEscapeState st = BridgeEscapeState.Turn1;
+
+		public BridgeEscapeScenario ()
+		{
+		}
+
+		public override bool run(Ev3System sys)
+		{
+			bool ret = false;
+
+			return ret;
+		}
+	}
+}
+
