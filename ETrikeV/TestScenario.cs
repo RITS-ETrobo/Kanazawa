c603f3764b524c79ef9cdf0c96eaf35b3b1b963d
kon <sound.recording.man@gmail.com>
2015-09-02 17:59:45 +0900

    PDをボタンで変更できるように修正

--- ETrikeV/TestScenario.cs
+++ ETrikeV/TestScenario.cs
@@ -1,24 +1,70 @@
 ﻿using System;
 using MonoBrickFirmware.Display;
+using MonoBrickFirmware.UserInput;
 
 namespace ETrikeV
 {
 	public class TestScenario : Scenario
 	{
-		private double kp = 4.5;
+		private double kp = 7.5;
 		private double ki = 0.0;
-		private double kd = 8.0;
+		private double kd = 0.0;
 		private double freq = 0.004;
-		private int cnt = 0;
 		private const double WHEEL_BASE = 212.0;
+
+		private int pos = 0;
 
 		private int[] diff = new int[2];
 		private double integral = 0;
 		private int speed = 0;
 
+		private bool log = false;
+
 		public TestScenario (int speed)
 		{
 			this.speed = speed;
+			ButtonEvents buts = new ButtonEvents();
+			buts.EnterPressed += () => {
+				log = true;
+			};
+			buts.UpPressed += () => {
+				switch (pos) {
+				case 0:
+					kp += 0.1;
+					LcdConsole.Clear();
+					LcdConsole.WriteLine("kp:" + kp);
+					break;
+				case 1:
+					kd += 0.1;
+					LcdConsole.Clear();
+					LcdConsole.WriteLine("kd:" + kd);
+					break;
+				default:
+					break;
+				}
+			};
+			buts.DownPressed += () => {
+				switch (pos) {
+				case 0:
+					kp -= 0.1;
+					LcdConsole.Clear();
+					LcdConsole.WriteLine("kp:" + kp);
+					break;
+				case 1:
+					kd -= 0.1;
+					LcdConsole.Clear();
+					LcdConsole.WriteLine("kd:" + kd);
+					break;
+				default:
+					break;
+				}
+			};
+			buts.LeftPressed += () => {
+				pos = (pos > 0) ? pos - 1 : 0;
+			};
+			buts.RightPressed += () => {
+				pos = (pos < 1) ? pos + 1 : 1;
+			};
 		}
 
 		private int pid(Ev3System sys, int light)
@@ -32,13 +78,16 @@ namespace ETrikeV
 
 			p = kp * diff [0];
 			i = ki * integral;
-			d = kd * (diff [0] - diff [1]) / freq;
+			d = kd * (diff [0] - diff [1]);
 
 			ret = (int)(p + i + d);
 
-			if (cnt++ % 120 == 0) {
+			if (log) {
+				log = false;
 				LcdConsole.Clear ();
-				LcdConsole.WriteLine ("PID : " + ret);
+				LcdConsole.WriteLine ("diff[0]:" + diff [0] + ", diff[1]:" + diff[1]);
+				LcdConsole.WriteLine ("p,i,d:" + p + ", " + i + ", " + d);
+				LcdConsole.WriteLine ("PID:" + ret);
 			}
 			ret = (ret > 100) ? 100 : ret;
 			ret = (ret < -100) ? -100 : ret;
@@ -66,14 +115,14 @@ namespace ETrikeV
 		{
 			int leftP, rightP;
 			int light = sys.colorRead ();
-			int pdVal = pid(sys, light);
+			int pidVal = pid(sys, light);
 
 			leftP = speed;
 			rightP = speed;
-			if (pdVal < 0) {
-				leftP -= leftP * pdVal * -1 / 100;
-			} else if (pdVal > 0) {
-				rightP -= rightP * pdVal / 100;
+			if (pidVal < 0) {
+				leftP -= leftP * pidVal * -1 / 100;
+			} else if (pidVal > 0) {
+				rightP -= rightP * pidVal / 100;
 			}
 
 			steerCtrl (sys, leftP, rightP);
