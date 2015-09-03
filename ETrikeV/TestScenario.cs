6e6c177d7985491da9cd9b4488ea5f66f5b38da8
kouta kon <sound.recording.man@gmail.com>
2015-09-04 01:02:29 +0900

    モーターテスト用の処理を追加

--- ETrikeV/TestScenario.cs
+++ ETrikeV/TestScenario.cs
@@ -6,125 +6,65 @@ namespace ETrikeV
 {
 	public class TestScenario : Scenario
 	{
-		private double kp = 7.5;
-		private double ki = 0.0;
-		private double kd = 0.0;
-		private double freq = 0.004;
-
-		private int pos = 0;
-
-		private int[] diff = new int[2];
-		private double integral = 0;
-		private int speed = 0;
-
-		private bool log = false;
-
-		public TestScenario (int speed)
+		public TestScenario ()
 		{
-			this.speed = speed;
-			ButtonEvents buts = new ButtonEvents();
-			buts.EnterPressed += () => {
-				log = true;
-			};
-			buts.UpPressed += () => {
-				switch (pos) {
-				case 0:
-					kp += 0.1;
-					LcdConsole.Clear();
-					LcdConsole.WriteLine("kp:" + kp);
-					break;
-				case 1:
-					kd += 0.1;
-					LcdConsole.Clear();
-					LcdConsole.WriteLine("kd:" + kd);
-					break;
-				default:
-					break;
-				}
-			};
-			buts.DownPressed += () => {
-				switch (pos) {
-				case 0:
-					kp -= 0.1;
-					LcdConsole.Clear();
-					LcdConsole.WriteLine("kp:" + kp);
-					break;
-				case 1:
-					kd -= 0.1;
-					LcdConsole.Clear();
-					LcdConsole.WriteLine("kd:" + kd);
-					break;
-				default:
-					break;
-				}
-			};
-			buts.LeftPressed += () => {
-				pos = (pos > 0) ? pos - 1 : 0;
-			};
-			buts.RightPressed += () => {
-				pos = (pos < 1) ? pos + 1 : 1;
-			};
-		}
-
-		private int pid(Ev3System sys, int light)
-		{
-			int ret = 0;
-			double p, i, d;
-
-			diff [1] = diff [0];
-			diff [0] = light - sys.TargetLight;
-			integral += (diff [0] + diff [1]) / 2 * freq;
-
-			p = kp * diff [0];
-			i = ki * integral;
-			d = kd * (diff [0] - diff [1]);
-
-			ret = (int)(p + i + d);
-
-			if (log) {
-				log = false;
-				LcdConsole.Clear ();
-				LcdConsole.WriteLine ("diff[0]:" + diff [0] + ", diff[1]:" + diff[1]);
-				LcdConsole.WriteLine ("p,i,d:" + p + ", " + i + ", " + d);
-				LcdConsole.WriteLine ("PID:" + ret);
-			}
-			ret = (ret > 100) ? 100 : ret;
-			ret = (ret < -100) ? -100 : ret;
-
-			return ret;
-		}
-
-		private void steerCtrl(Ev3System sys, int leftPwr, int rightPwr)
-		{
-			int target = SteerCtrl.getSteeringAngle (leftPwr, rightPwr);
-			target *= 8; // degree -> tacho
-			int current = sys.steerGetTachoCount ();
-			int diff = (target - current) * 2;
-			diff = (diff > 100) ? 100 : diff;
-			diff = (diff < -100) ? -100 : diff;
-			sys.setSteerPower (diff);
-
-			return;
+			
 		}
 
 		public override bool run(Ev3System sys)
 		{
-			int leftP, rightP;
-			int light = sys.colorRead ();
-			int pidVal = pid(sys, light);
-
-			leftP = speed;
-			rightP = speed;
-			if (pidVal < 0) {
-				leftP -= leftP * pidVal * -1 / 100;
-			} else if (pidVal > 0) {
-				rightP -= rightP * pidVal / 100;
+			int startTachoCount = 0;
+			int currentTachoCount = 0;
+			motorA = new Motor(MotorPort.OutA);
+			motorB = new Motor(MotorPort.OutB);
+			motorC = new Motor(MotorPort.OutC);
+			motorD = new Motor(MotorPort.OutD);
+			
+			startTachoCount = motorA.GetTachoCount();
+			motorA.SetPower(100);
+			while (true) {
+				currentTachoCount = motorA.GetTachoCount();
+				if (currentTachoCount > startTachoCount + 5000) {
+					break;
+				}
+				motorA.Brake();
+				LcdConsole.WriteLine("MotorA : ");
 			}
-
-			steerCtrl (sys, leftP, rightP);
-			sys.setLeftMotorPower (leftP);
-			sys.setRightMotorPower (rightP);
-
+			
+			startTachoCount = motorB.GetTachoCount();
+			motorB.SetPower(100);
+			while (true) {
+				currentTachoCount = motorB.GetTachoCount();
+				if (currentTachoCount > startTachoCount + 5000) {
+					break;
+				}
+				motorB.Brake();
+				LcdConsole.WriteLine("MotorB : ");
+			}
+			
+			startTachoCount = motorC.GetTachoCount();
+			motorC.SetPower(100);
+			while (true) {
+				currentTachoCount = motorC.GetTachoCount();
+				if (currentTachoCount > startTachoCount + 5000) {
+					break;
+				}
+				motorC.Brake();
+				LcdConsole.WriteLine("MotorC : ");
+			}
+			
+			startTachoCount = motorD.GetTachoCount();
+			motorD.SetPower(100);
+			while (true) {
+				currentTachoCount = motorD.GetTachoCount();
+				if (currentTachoCount > startTachoCount + 5000) {
+					break;
+				}
+				motorD.Brake();
+				LcdConsole.WriteLine("MotorD : ");
+			}
+			
+			Thred.Sleep(10000);
 			return false;
 		}
 	}
